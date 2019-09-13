78aa35201e5ee2a7636f93878a4313b4ae042f3f
Asef Ahmed <asefahmed56@gmail.com>
2019-09-13 14:00:07 -0400

    remove explicit references to generic types

--- src/EllCrv/EllCrvModel.jl
+++ src/EllCrv/EllCrvModel.jl
@@ -21,8 +21,8 @@ export EllCrvModel
 #
 ################################################################################
 
-mutable struct EllCrvModel{T, P_1,...,P_s}
-    elliptic_curve::EllCrv{T}
-    ec_point::EllCrvPt{T}
-    divisor::EllCrvDivisor{T, P_1,...,P_s}
+mutable struct EllCrvModel
+    elliptic_curve::EllCrv
+    ec_point::EllCrvPt
+    divisor::EllCrvDivisor
 end

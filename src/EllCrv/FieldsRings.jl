06b5556e9a3a56b7d79cedc2844600c6ef6f6224
Asef Ahmed <asefahmed56@gmail.com>
2019-09-13 13:58:45 -0400

    add notice, conditions, disclaimer from Hecke.jl

--- src/EllCrv/FieldsRings.jl
+++ src/EllCrv/FieldsRings.jl
@@ -4,6 +4,35 @@
 #           Field Extensions --> Number Fields
 #           Local Rings --> Function Fields
 #
+# This file is part of Hecke.
+#
+# Copyright (c) 2015, 2016: Claus Fieker, Tommy Hofmann
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# (C) 2016 Tommy Hofmann
+# (C) 2016 Robin Ammon
+# (C) 2016 Sofia Brenner
+#
 ################################################################################
 
 
@@ -126,7 +155,7 @@ end
 
 
 # Function fields of curves.
-mutable struct FuncField{T} <: Loc
+mutable struct FuncField <: Loc
     # The function field K[E] of the elliptic curve E 
     # consists of ratios of polynomials. Taking an 
     # elliptic curve E as an integral, regular scheme of 
@@ -134,7 +163,7 @@ mutable struct FuncField{T} <: Loc
     # just the local ring of the generic point.
     
     base_curve::EllCrv
-    generic_point::EllCrvPt # Scheme-theoretic generic point -- to implement
+    generic_point::EllCrvPt
 end
 
 ###############################################################################

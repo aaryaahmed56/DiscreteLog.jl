3d7ef1272fcdfeeb41a452552681b950d3b5c57f
Asef Ahmed <asefahmed56@gmail.com>
2019-09-16 16:49:35 -0400

    add map type parent, add curve map struct, methods

--- src/EllCrv/EllCrvMap.jl
+++ src/EllCrv/EllCrvMap.jl
@@ -29,22 +29,52 @@ import AbstractAlgebra
 #
 ################################################################################
 
-export AbstractVarietyMap, EllCrvMap, EllCrvBlowup
+export AbstractVarietyMap, AbstractCurveMap, EllCrvBlowup, EllCrvMap
 
 ################################################################################
 #
 #  Abstract Types/Structs
 #
 ################################################################################
+Map(::Type{T}) where T <: AbstractAlgebra.Map = AbstractAlgebra.Map(T)
 
-mutable struct AbstractVarietyMap
-    source, target::Tuple{AbstractVariety, AbstractVariety}
+mutable struct AbstractVarietyMap <: AbstractAlgebra.Map{AbstractVariety, 
+    AbstractVariety}
+    source::AbstractVariety
+    target::AbstractVariety
+
+    function AbstractVarietyMap(X::AbstractVariety, Y::AbstractVariety)
+        z = new(source, target)
+        return z
+    end
 end
 
-mutable struct EllCrvMap <: AbstractVarietyMap
-    source, target::Tuple{EllCrv, EllCrv}
+mutable struct AbstractCurveMap <: AbstractVarietyMap
+    source::AbstractCurve
+    target::AbstractCurve
+
+    function AbstractCurveMap(X::AbstractCurve, Y::AbstractCurve)
+        z = new(source, target)
+        return z
+    end
 end
 
-mutable struct EllCrvBlowup{i, e} <: EllCrvMap
-    source, target::Tuple{i::AbstractVariety, e::EllCrv}
+mutable struct EllCrvBlowup <: AbstractVarietyMap
+    source::AbstractVariety
+    target::EllCrv
+
+    function EllCrvBlowup(Z::AbstractVariety, Y::EllCrv)
+        z = new(source, target)
+        return z
+    end
+end
+
+mutable struct EllCrvMap <: AbstractCurveMap
+    source::EllCrv
+    target::EllCrv
+
+    function EllCrvMap(E1::EllCrv, E2::EllCrv)
+        z = new(source, target)
+        return z
+    end
 end
